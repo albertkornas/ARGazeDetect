a53d5e9fd46d6cce8cdf1b3f3875f497d86c97c7
Albert Kornas <albertkornas@gmail.com>
2021-12-10 20:47:22 -0500

    Initial Commit

---
+++ ARGazeDetect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARGazeDetect
+//
+//  Created by Albert Kornas on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
