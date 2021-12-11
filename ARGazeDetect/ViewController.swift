f723b15f42f4d461633b77f171aeb72dcf7754d3
Albert Kornas <albertkornas@gmail.com>
2021-12-11 00:50:47 -0500

    Plays a video and detects when users eyes are away from the screen in order to pause the ad and notify user to watch the screen

--- ARGazeDetect/ViewController.swift
+++ ARGazeDetect/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import ARKit
+import AVKit
 import SceneKit
 
 class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
@@ -23,6 +24,7 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
             }
         }
     }
+    var avPlayer: AVPlayer!
     
     public var positionLabel: UILabel = UILabel(frame: CGRect(x: 100, y: 100, width: 150, height: 100))
     
@@ -35,6 +37,24 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
         sceneView.automaticallyUpdatesLighting = true
         positionLabel.text = "Vision location"
         sceneView.addSubview(positionLabel)
+        //sceneView.scene.background.contents = UIColor.clear
+        
+        let filepath: String? = Bundle.main.path(forResource: "ad", ofType: "mp4")
+               let fileURL = URL.init(fileURLWithPath: filepath!)
+
+               avPlayer = AVPlayer(url: fileURL)
+        
+               let avPlayerController = AVPlayerViewController()
+               avPlayerController.player = avPlayer
+               avPlayerController.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+
+               // Turn on video controlls
+               avPlayerController.showsPlaybackControls = true
+
+               // play video
+               avPlayerController.player?.play()
+               self.view.addSubview(avPlayerController.view)
+               self.addChild(avPlayerController)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -79,9 +99,18 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
             let contentNode = contentController.contentNode else {
             return
         }
+        
         DispatchQueue.main.async {
             let positionX = contentController.visionPosition[0]+0.5
             let positionY = 1-(contentController.visionPosition[1]+0.5)
+            if positionX.magnitude <= 0.4 || positionX.magnitude >= 0.6 {
+                let alert = UIAlertController(title: "Ad Paused", message: "You must watch the advertisement to proceed.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .default))
+                self.present(alert, animated: true)
+                self.avPlayer.pause()
+            } else {
+                self.avPlayer.play()
+            }
             self.positionLabel.frame = CGRect(x: CGFloat(positionX) * self.sceneView.frame.width, y: CGFloat(positionY) * self.sceneView.frame.height, width: 150, height: 100)
         }
         contentController.renderer(renderer, didUpdate: contentNode, for: anchor)
